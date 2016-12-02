afe40f45ffcd6dcc1ca5c8ca600891dbdbd2b502
dwityaco <dwityaco@leona.rapidplex.com>
2016-12-03 05:13:04 +0700

    basic layout

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="/public/img/cm-ico.png">
+        <title>Arts</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="/public/css/apps.css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway';
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    <a href="{{ url('/login') }}">Login</a>
+                    <a href="{{ url('/register') }}">Register</a>
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Welcome
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div> -->
+		<div class="container-fluid film-box">
+			<div class="row">
+				<!-- Navigation Menu -->
+				<div class="navbar">
+					<div class="col-xs-2">
+						<div class="navbar-left col-xs-12"> ARTS </div>
+						<div class="navbar-left-sub col-xs-12"> ARTS </div>
+					</div>
+					<div class="col-xs-4">
+						<div class="navbar-left-sub col-xs-12"> Lorem ipsum dolor sit amet </div>
+					</div>
+				</div>
+				<!-- Menu -->
+				<div class="col-xs-12 menu-box">
+					<div class="col-xs-4">
+						<div class="col-xs-12 menu-cards pixel-card">
+							Pixel Arts
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="col-xs-12 menu-cards photo-card">
+							Photography
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="col-xs-12 menu-cards craft-card">
+							Craft
+						</div>
+					</div>
+				</div>
+				<!-- Footer -->
+				<div class="col-xs-12 footer-text">
+					<p>
+					Wates Street KM 9,5 <br>
+					Sedayu <br>
+					Bantul <br>
+					</p>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
